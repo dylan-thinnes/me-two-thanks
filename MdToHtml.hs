d0d3f1eda57342d55c892001786cefbc6e1f2d07
dylan-thinnes <dylan.thinnes@protonmail.com>
2018-09-28 22:25:38 +0100

    Begin work on HTML DSL

--- MdToHtml.hs
+++ MdToHtml.hs
@@ -16,6 +16,8 @@ import Data.Text                    (Text)
 newtype Html     = Html     { fromHtml :: Text }
     deriving (Show, Read)
 
+-- Conversion Systems
+
 convertMarkdownToHtml :: Text -> Either PandocError Html
 convertMarkdownToHtml t = runPure $ readMarkdown def t >>= writeHtml5String def >>= return . Html
 
@@ -26,3 +28,8 @@ convertMarkdownToHtmlSafe :: Text -> Html
 convertMarkdownToHtmlSafe t = case convertMarkdownToHtml t of
     Right html -> html
     Left  _    -> Html "Parsing error in Markdown."
+
+-- Simple HTML DSL
+data Attribute = Attribute { key :: Text, value :: Text }
+data Node = Element { name :: Text, attributes :: [Attribute], children :: [Node] }
+          | TextNode Text
