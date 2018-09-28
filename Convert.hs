79e7730d0435060b323413f368429f93c648df41
dylan-thinnes <dylan.thinnes@protonmail.com>
2018-09-28 21:18:20 +0100

    Add deprecation to project listings

--- Convert.hs
+++ Convert.hs
@@ -68,6 +68,9 @@ articleToBlurb (Article title author date description content) = convertMarkdown
 data Project = Project T.Text T.Text T.Text
     deriving (Show, Read)
 
+deprecated :: Project -> Bool
+deprecated (Project _ url _) = T.head url == '#'
+
 parseProject :: T.Text -> Project
 parseProject t = Project name url description
     where
@@ -78,13 +81,16 @@ parseProjects :: T.Text -> [Project]
 parseProjects = map parseProject . T.splitOn "\n\n"
 
 projectToHtml :: Project -> Html
-projectToHtml (Project name url description) = convertMarkdownToHtmlSafe $ Markdown $ T.concat [nameMd, "  \n", description]
+projectToHtml p@(Project name url description) = convertMarkdownToHtmlSafe $ Markdown $ T.concat [nameMd, deprecationMd, "  \n", description]
     where
     nameMd = T.concat ["**", name, "**"]
+    deprecationMd = if deprecated p then " (Deprecated)" else ""
 
 projectsToHtml :: [Project] -> Html
 projectsToHtml ps = Html . T.concat $ (title:wrappedProjects)
     where
     title = html $ convertMarkdownToHtmlSafe $ Markdown "## Projects"
-    wrapProject p@(Project _ url _) = T.concat ["<a href=\"", url, "\">", (html . projectToHtml) p , "</a>"]
+    wrapProject p@(Project _ url _) = if deprecated p
+        then T.concat ["<div>",                  (html . projectToHtml) p , "</div>"]
+        else T.concat ["<a href=\"", url, "\">", (html . projectToHtml) p , "</a>"]
     wrappedProjects = map wrapProject ps
