b8637731aec364498bd277142e4c6741869f6a39
dylan-thinnes <dylan.thinnes@protonmail.com>
2018-09-29 09:16:23 +0100

    Complete Html DSL

--- MdToHtml.hs
+++ MdToHtml.hs
@@ -11,25 +11,40 @@ import Text.Pandoc.Readers.Markdown (readMarkdown)
 import Text.Pandoc.Writers.HTML     (writeHtml5String)
 import Text.Pandoc.Error            (PandocError, handleError)
 import Data.Default                 (def)
-import Data.Text                    (Text)
+import qualified Data.Text as T
 
-newtype Html     = Html     { fromHtml :: Text }
+newtype Html     = Html     { fromHtml :: T.Text }
     deriving (Show, Read)
 
 -- Conversion Systems
 
-convertMarkdownToHtml :: Text -> Either PandocError Html
+convertMarkdownToHtml :: T.Text -> Either PandocError Html
 convertMarkdownToHtml t = runPure $ readMarkdown def t >>= writeHtml5String def >>= return . Html
 
-convertMarkdownToHtmlIO :: Text -> IO Html
+convertMarkdownToHtmlIO :: T.Text -> IO Html
 convertMarkdownToHtmlIO t = handleError $ convertMarkdownToHtml t
 
-convertMarkdownToHtmlSafe :: Text -> Html
+convertMarkdownToHtmlSafe :: T.Text -> Html
 convertMarkdownToHtmlSafe t = case convertMarkdownToHtml t of
     Right html -> html
     Left  _    -> Html "Parsing error in Markdown."
 
 -- Simple HTML DSL
-data Attribute = Attribute { key :: Text, value :: Text }
-data Node = Element { name :: Text, attributes :: [Attribute], children :: [Node] }
-          | TextNode Text
+data Attribute = Attribute { key :: T.Text, value :: T.Text }
+data Node = Element { name :: T.Text, attributes :: [Attribute], children :: [Node] }
+          | TextNode T.Text
+          | RawHtml T.Text
+
+showText :: Node -> T.Text
+showText (Element name attributes children) = T.concat [openingTag, childrenT, closingTag]
+    where
+    openingTag  = T.concat $ ["<", name, attributesT, ">"]
+    attributesT = T.concat $ map (\(Attribute k v) -> T.concat [" ", k, "=\"", v, "\""]) attributes
+    childrenT   = T.concat $ map showText children
+    closingTag  = T.concat $ ["</", name, ">"]
+
+showText (TextNode t) = t
+showText (RawHtml t) = t
+
+instance Show Node where
+    show = T.unpack . showText
